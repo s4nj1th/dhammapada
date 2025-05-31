9336dd85f05e5a7a65de4de832ddf887b4fd057a
s4nj1th <sanjith@tutamail.com>
2025-06-01 02:39:00 +0530

    new version

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,7 +7,7 @@ import '../providers/translations_provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
-  static const appVersion = '1.1.0';
+  static const appVersion = '1.1.1';
 
   @override
   Widget build(BuildContext context) {
