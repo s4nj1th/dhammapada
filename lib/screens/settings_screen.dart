0a053984a250ea6c055ac5960c9bde2e7a333be2
s4nj1th <sanjith@tutamail.com>
2025-06-01 00:24:45 +0530

    v1.1.0 > update version

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -7,6 +7,7 @@ import '../providers/translations_provider.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
+  static const appVersion = '1.1.0';
 
   @override
   Widget build(BuildContext context) {
@@ -83,7 +84,7 @@ class SettingsScreen extends StatelessWidget {
               showAboutDialog(
                 context: context,
                 applicationName: 'PocketDhamma',
-                applicationVersion: '0.0.0',
+                applicationVersion: appVersion,
               );
             },
           ),
